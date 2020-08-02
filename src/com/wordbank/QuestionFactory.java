e5122d5592e040633bc7539a2768d79158ce5069
rajantamang1 <62918494+rajantamang1@users.noreply.github.com>
2020-08-01 14:28:40 -1000

    Added QuestionFactory

---
+++ src/com/wordbank/QuestionFactory.java
@@ -0,0 +1,15 @@
+package com.wordbank;
+
+public class QuestionFactory {
+
+    // instances
+    private char firstLetter;
+
+
+    // method to generate a random question
+    public String generateQuestion(){
+        String question="";
+
+        return question;
+    }
+}
--- src/com/wordbank/Word.java
+++ src/com/wordbank/Word.java
@@ -1,9 +1,11 @@
 package com.wordbank;
 
-public class Word {
+import java.util.Comparator;
+import java.util.List;
+
+public class Word implements Comparator {
     // instance- variables, fields and attributes
     private  int lengthOfWords; // number of characters of words
-    private char firstLetter; // starting character fo word for question
 
     // Enum instance
     private Level level = Level.EASY;
@@ -22,6 +24,7 @@ public class Word {
         return result;
     }
 
+
     // Accessor method fro private fields
     public int getLengthOfWords() {
         return lengthOfWords;
@@ -31,13 +34,7 @@ public class Word {
         this.lengthOfWords = lengthOfWords;
     }
 
-    public char getFirstLetter() {
-        return firstLetter;
-    }
 
-    public void setFirstLetter(char firstLetter) {
-        this.firstLetter = firstLetter;
-    }
 
     public Level getLevel() {
         return level;
@@ -45,5 +42,12 @@ public class Word {
 
     public void setLevel(Level level) {
         this.level = level;
+    }
+
+
+    // Method to compare
+    @Override
+    public int compare(Object o1, Object o2) {
+        return 0;
     }
 }
