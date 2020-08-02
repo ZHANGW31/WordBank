77c2b3160afea2cb7c0c5fbbd5e907a8d0bf5b52
rajantamang1 <62918494+rajantamang1@users.noreply.github.com>
2020-08-01 14:46:39 -1000

    Added QuestionFactory/ with random question generator/ sample run in Game.Java

--- src/com/wordbank/QuestionFactory.java
+++ src/com/wordbank/QuestionFactory.java
@@ -1,15 +1,25 @@
 package com.wordbank;
 
+import java.util.Random;
+
 public class QuestionFactory {
 
     // instances
     private char firstLetter;
-
+    private String question= " What is the word that starts with:";
 
     // method to generate a random question
     public String generateQuestion(){
         String question="";
 
         return question;
+    }
+
+    public String getRandomQuestion(){
+
+        Random random = new Random();
+        char c = (char)(random.nextInt(26)+ 'a');
+
+        return question + c;
     }
 }
--- src/com/wordbankclient/Game.java
+++ src/com/wordbankclient/Game.java
@@ -1,11 +1,15 @@
 package com.wordbankclient;
 
+import com.wordbank.QuestionFactory;
+
 public class Game {
     public static void main(String[] args) {
-
+        QuestionFactory questionFactory = new QuestionFactory();
 
         System.out.println("Welcome to the $WordBank$ heist game!");
-
+        for(int i =0; i<10; i++) {
+            System.out.println(questionFactory.getRandomQuestion());
+        }
     }
 
 }
