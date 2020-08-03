1a908c98a9a97014d1ed1b56729d9fa69191d7cc
rajantamang1 <62918494+rajantamang1@users.noreply.github.com>
2020-08-03 11:31:09 -1000

    made change in QuestionFactory

---
+++ src/com/wordbank/client/Game.java
@@ -0,0 +1,64 @@
+package com.wordbank.client;
+
+import com.wordbank.QuestionFactory;
+import com.wordbank.WordBankCollection;
+
+
+import java.io.Console;
+import java.io.File;
+
+public class Game {
+    QuestionFactory questionFactory = new QuestionFactory();
+    Console console = System.console();
+    WordBankCollection wordBankCollection ;
+    public void start(){
+        System.out.println("Welcome to the $WordBank$ heist game!");
+
+        System.out.println("Guess a word that you think is in the WordBank, starts with following Character");
+
+        File file = new File("\\Welcome_Banner.txt");
+        System.out.println(file);
+    }
+
+    public void askQuestion(){
+        System.out.println(questionFactory.getRandomQuestion()+ ". The word has"+ questionFactory.getRandomNumber(1,3)+
+                "character");
+    }
+
+    public void correctAnswer(){
+        if(console.readLine().equals(wordBankCollection.getAllWords())){
+            console.printf("Correct Answer, You have own"+ console.readLine().length()*100 +"dollars");
+        } else{
+            console.printf("Try again");
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+
+        Game game = new Game();
+        game.start();
+        game.correctAnswer();
+
+
+
+
+
+
+
+
+        //String userInput = scanner.nextLine();
+
+       // Player player = new Player();
+        //String playerInput = player.answerTheQuestion(scanner.nextLine()); // allows player to input word
+
+        // TODO: for certain level, if player answer is correct, player cashbalance will be added
+        /*if((level.getValue()) && playerInput.length()<=3 ){
+            player.cashOut();
+        }*/
+
+
+    }
+
+}
